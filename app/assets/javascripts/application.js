43e62537410bf36ff3c7d822b0650436c0476a15
abbacdef <coincash@mail.ru>
2018-12-06 17:55:22 +0500

    new

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require rails-ujs
+//= require bootstrap
+//= require_tree .
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+//= require rails-ujs
